019132f12797ef1c4a781dbecafd8fa360ed7cca
Faza <firdausauliafaza@gmail.com>
2021-12-31 21:07:17 +0700

    First
    Rancang DB + Login

---
+++ app/Http/Livewire/Console/Logout.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Console;
+
+use Livewire\Component;
+
+class Logout extends Component
+{
+    public function render()
+    {
+        return view('livewire.console.logout');
+    }
+}
